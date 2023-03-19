5c0030dc81682132d96579a1184dc9344ecbb500
MalachiNibbs <malachinibbs@gmail.com>
2023-03-19 00:59:48 -0400

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Otro on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
